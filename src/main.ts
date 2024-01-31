8d15f019d34f8768ad8dd8a397297bca6e8d833b
{Marina <{akflsk6040@gmail.com}>
2024-02-01 02:25:19 +0900

    Add Matter.js Boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
 import './style.css';
+import { Engine, Render, Runner, Composite, Bodies } from 'matter-js';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>두 번째 배포</h1>
-  </div>
-`;
+const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
+const [width, height] = [canvas.offsetWidth, canvas.offsetHeight];
+
+const engine = Engine.create();
+engine.gravity.y = 0;
+const world = engine.world;
+const runner = Runner.create({ isFixed: true });
+const render = Render.create({
+	engine,
+	canvas,
+	options: {
+		width: canvas.offsetWidth,
+		height: canvas.offsetHeight,
+		wireframes: false,
+	},
+});
+
+Render.run(render);
+
+Runner.run(runner, engine);
+
+Composite.add(world, [
+	Bodies.rectangle(-2.5, height / 2, 5, height, { isStatic: true }),
+	Bodies.rectangle(width + 2.5, height / 2, 5, height, { isStatic: true }),
+	Bodies.rectangle(width / 2, -2.5, width, 5, { isStatic: true }),
+	Bodies.rectangle(width / 2, height + 2.5, width, 5, { isStatic: true }),
+]);
